5267e488854fcd1b391f29c9ee26bfbcd4458cff
Felipe <fmoralesch@outlook.com>
2024-04-09 16:35:00 +0000

    first commit

---
+++ src/addTask.js
@@ -0,0 +1,20 @@
+const { v4 } = require('uuid')
+const AWS = require('aws-sdk')
+
+module.exports.addTask = async (event) => {
+    const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+    const {title, description} = JSON.parse(event.body)
+    const createdAt = new Date()
+    const id = v4()
+
+    const newTask = {
+        id, title, description, createdAt
+    }
+    await dynamodb.put({
+        TableName: 'TaskTable',
+        Item: newTask
+    }).promise()
+
+    return {statusCode: 200, body: JSON.stringify(newTask)}
+}
